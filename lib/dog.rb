14d713269616e4eb28a84b0a762f51a82ce0daf0
billymwangi <billy.mwangi@student.moringaschool.com>
2022-08-16 16:54:04 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+    def name=(name)
+        @name= name
+    end
+    def name
+        @name
+    
+    def breed=(breed)
+        @breed=breed
+    end
+    def breed 
+        @breed
+    end
 end
+
